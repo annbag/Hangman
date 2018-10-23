27ddb378e51d5f78444362c32b6438f0e2f3f919
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-23 22:52:18 +0200

    add variable and get elements

---
+++ script.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let categories; // array of topics
+  let chosenCategory; // selected catagory
+  let word; // selected word
+  let guess; //guess
+  let guesses = []; // stored guesses
+  let lives; // lives
+  let counter; // count correct guesses
+  let space; // number of spaces in word
+
+  //get elements
+  let showLives = document.getElementById('lives');
+  let showCategory = document.getElementById('category');
+
+  //create alphabet
+
+  //select category
+  
+  //create guesses
+
+  //show lives
+  
+  //animate man
+  
+  //hangman
+
+  //onclick function
+  //play
+  //reset
+}
